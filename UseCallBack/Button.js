086a071a65c3072960bcd035f5e0501943c1ee51
ynaveenkmr <ynaveenkmr@gmail.com>
2020-08-10 09:40:58 +0530

    added Button for useCallBack hook

---
+++ UseCallBack/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Button({handleClick, Children}){
+  console.log('Rendering Button ', Children)
+  return(
+    <>
+    <div>
+    <Button onClick={handleClick}>{Children}</Button>
+    </div> 
+    </>
+  )
+}
